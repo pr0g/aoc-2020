24b827b86f6562c4487957f078e388eb4eba6cc9
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-02 21:20:37 +0000

    day 1, part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -16,11 +16,25 @@ int main(int argc, char** argv) {
             if (result == 2020) {
                 std::cout << numbers[i] << " and " << numbers[j] << "\n";
                 std::cout << numbers[i] * numbers[j] << "\n";
-                goto end;
+                goto end_part1;
             }
         }
     }
-end:
+end_part1:
+
+    for (int i = 0; i < numbers.size(); ++i) {
+        for (int j = i + 1; j < numbers.size(); ++j) {
+            for (int k = j + 1; k < numbers.size(); ++k) {
+                int result = numbers[i] + numbers[j] + numbers[k];
+                if (result == 2020) {
+                    std::cout << numbers[i] << ", " << numbers[j] << " and " << numbers[k] << "\n";
+                    std::cout << numbers[i] * numbers[j] * numbers[k] << "\n";
+                    goto end_part2;
+                }
+            }
+        }
+    }
+end_part2:
 
     return 0;
 }
