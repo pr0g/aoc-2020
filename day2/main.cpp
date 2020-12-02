e64367b16b1bed2da1f094f530cef32e9de38378
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-02 21:50:17 +0000

    day2 part 2 complete

--- day2/main.cpp
+++ day2/main.cpp
@@ -13,21 +13,30 @@ int main(int argc, char** argv) {
     char character;
     std::string password;
 
-    int valid_count = 0;
+    int valid_count_part1 = 0;
+    int valid_count_part2 = 0;
     for (;reader >> begin >> blank >> end >> character >> blank >> password;) {
-        std::cout << begin << " " << end << " " << character << " " << password << "\n";
-
         const auto count = std::count_if(
             password.cbegin(), password.cend(), [character](const char c) {
             return c == character;
         });
 
         if (count >= begin && count <= end) {
-            valid_count++;
+            valid_count_part1++;
+        }
+
+        auto begin_lookup = begin - 1;
+        auto end_lookup = end - 1;
+
+        if ((password[begin_lookup] == character
+            || password[end_lookup] == character)
+            && password[begin_lookup] != password[end_lookup]) {
+            valid_count_part2++;
         }
     }
 
-    std::cout << valid_count << "\n";
+    std::cout << valid_count_part1 << "\n";
+    std::cout << valid_count_part2 << "\n";
 
     return 0;
 }
