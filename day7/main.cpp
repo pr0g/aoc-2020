ac1652969e7e8961d5ac93d654ee64acfb0b0b2e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-07 22:52:23 +0000

    day 7 beginning data structure

--- day7/main.cpp
+++ day7/main.cpp
@@ -6,6 +6,26 @@
 #include <sstream>
 #include <array>
 #include <numeric>
+#include <unordered_map>
+
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
 
 int main(int argc, char** argv) {
     std::ifstream reader("input.txt");
@@ -17,10 +37,6 @@ int main(int argc, char** argv) {
 
     auto bag_finder =
         [](const std::string::size_type start, const std::string& line) {
-            if (start >= line.size()) {
-                return std::string::npos;
-            }
-            
             if (auto comma_offset = line.find(",", start);
                 comma_offset != std::string::npos) {
                 return comma_offset;
@@ -28,17 +44,25 @@ int main(int argc, char** argv) {
                       period_offset != std::string::npos) {
                 return period_offset;
             }
-
             return std::string::npos;
         };
 
-    using namespace std::string_literals;
+    using bags = std::pair<std::string, int>;
+    std::unordered_map<std::string, std::vector<bags>> all_bags;
+
+    using std::string_literals::operator""s;
     for (const auto& line : lines) {
         if (const auto contains_found = line.find("contain");
             contains_found != std::string::npos) {
             const std::string bag = line.substr(0, contains_found);
-            const std::string color = bag.substr(0, bag.find("bag"));
+            std::string color = bag.substr(0, bag.find("bag"));
+            trim(color);
             std::cout << color << "contains:\n";
+            
+            if (auto it = all_bags.find(color);
+                it == all_bags.end()) {
+                all_bags.insert({color, std::vector<bags>{}});
+            }
 
             if (const auto no_found = line.find("no", contains_found);
                 no_found != std::string::npos) {
@@ -60,11 +84,30 @@ int main(int argc, char** argv) {
 
                     auto next_bag = line.substr(next_bag_begin_offset, next_bag_end_offset - next_bag_begin_offset);
                     auto next_color = next_bag.substr(0, next_bag.find("bag"));
+                    trim(next_color);
                     std::cout << next_color << "\n";
+
+                    if (auto it = all_bags.find(color);
+                        it != all_bags.end()) {
+                        it->second.push_back(std::make_pair(next_color, stoi(count)));
+                    }
 
                     count_offset = next_bag_end_offset + 2;
                 }
             }
+        }
+    }
+
+    std::cout << all_bags.size() << "\n";
+
+    for (const auto& bags : all_bags["light red"]) {
+        std::cout << bags.first << ":" << bags.second << "\n";
+    }
+
+    for (auto it : all_bags) {
+        std::cout << it.first << "\n";
+        for (auto in : it.second) {
+            std::cout << "  " << in.first << " -- " << in.second << "\n";
         }
     }
 
