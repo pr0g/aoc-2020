26d335581a090624c1117bec88a11bc2a74a83c7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-09 20:02:07 +0000

    day 8 part 2

--- day8/main.cpp
+++ day8/main.cpp
@@ -13,16 +13,16 @@
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-struct nop_t {};
+struct nop_t { int value; };
 struct acc_t { int value; };
-struct jmp_t { int offset; };
+struct jmp_t { int value; };
 
 using instruction_t = std::variant<std::monostate, nop_t, acc_t, jmp_t>;
 
 instruction_t decode_instruction(
     const std::string& opcode, const std::string& value) {
     if (opcode == "nop") {
-        return nop_t{};
+        return nop_t{ stoi(value) };
     } else if (opcode == "acc") {
         return acc_t { stoi(value) };
     } else if (opcode == "jmp") {
@@ -59,7 +59,7 @@ int main(int argc, char** argv) {
         std::visit(overloaded {
             [](nop_t arg) { std::cout << "nop\n"; },
             [](acc_t arg) { std::cout << "acc:" << arg.value << "\n"; },
-            [](jmp_t arg) { std::cout << "jmp:" << arg.offset << "\n"; },
+            [](jmp_t arg) { std::cout << "jmp:" << arg.value << "\n"; },
             [](std::monostate arg) { std::cout << "unhandled\n"; }
         }, instruction);
     }
@@ -69,31 +69,64 @@ int main(int argc, char** argv) {
         int acc = 0;
     };
 
-    std::vector<int> memory(instructions.size(), 0);
-    auto check = [&](program_t prog) { 
+    auto check = [&](program_t prog, std::vector<int>& memory) { 
         int value = memory[prog.ip];
         if (value > 0) {
-            std::cout << "part1: " << prog.acc << "\n";
+            // enable for part 1
+            // std::cout << "part1: " << prog.acc << "\n";
             return false;
         }
         memory[prog.ip]++;
         return true;
     };
 
-    program_t program;
-    for (;;) {
-        const auto instruction = instructions[program.ip];
-        auto okay = std::visit(overloaded {
-            [&](nop_t arg) { program.ip++; return check(program); },
-            [&](acc_t arg) { program.acc += arg.value; program.ip++; return check(program); },
-            [&](jmp_t arg) { program.ip += arg.offset; return check(program); },
-            [](std::monostate arg) { std::cout << "unhandled\n";  return false; }
-        }, instruction);
+    int jmp_change_index = 0;
+    int nop_change_index = 0;
 
-        if (!okay) {
-            break;
+    program_t program;
+    for (bool valid_program = false, loop_detected = false; !valid_program && !loop_detected;) {
+        std::vector<int> memory(instructions.size(), 0);
+
+        std::vector instructions_copy = instructions;
+        // enable for part 2
+        if (auto it = std::find_if(
+            instructions.begin() + jmp_change_index, instructions.end(),
+            [](const auto& inst) {
+                return std::holds_alternative<jmp_t>(inst);
+            });
+            it != instructions.end()) {
+            const auto offset = it - instructions.begin();
+            instruction_t inst = *it;
+            if (auto jmp = std::get_if<jmp_t>(&inst)) {
+                instructions_copy[offset] = nop_t{ jmp->value };
+            }
+            jmp_change_index = offset + 1;
+        }
+
+        program = {};
+        for (;;) {
+            const auto instruction = instructions_copy[program.ip];
+            auto okay = std::visit(overloaded {
+                [&](nop_t arg) { program.ip++; return check(program, memory); },
+                [&](acc_t arg) { program.acc += arg.value; program.ip++; return check(program, memory); },
+                [&](jmp_t arg) { program.ip += arg.value; return check(program, memory); },
+                [](std::monostate arg) { std::cout << "unhandled\n";  return false; }
+            }, instruction);
+
+            if (!okay) {
+                // enable for part 1
+                // loop_detected = true;
+                break;
+            }
+
+            if (program.ip >= instructions.size()) {
+                valid_program = true;
+                break;
+            }
         }
     }
+
+    std::cout << "part2: " << program.acc << "\n";
 
     return 0;
 }
