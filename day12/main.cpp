58782df8276e6c3139aaf10aa959b24a524b35b2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-12 16:09:16 +0000

    day 12 part 2

--- day12/main.cpp
+++ day12/main.cpp
@@ -26,6 +26,12 @@ enum class rotation_e
     two_seventy = 3
 };
 
+struct waypoint_t
+{
+    int x;
+    int y;
+};
+
 struct ship_t
 {
     heading_e heading;
@@ -38,6 +44,20 @@ struct control_t
     char action;
     int value;
 };
+
+double deg_to_rad(double degrees) {
+    return (3.14159265359 / 180.0) * degrees;
+}
+
+waypoint_t rotate(int angle, int direction, int xx, int yy) {
+    double a = deg_to_rad(double(angle * direction));
+    double xd = double(xx);
+    double yd = double(yy);
+    double x = (xd * std::cos(a) - yd * std::sin(a));
+    double y = (xd * std::sin(a) + yd * std::cos(a));
+
+    return waypoint_t{int(std::round(x)), int(std::round(y))};
+}
 
 rotation_e rotation(int value) {
     switch (value)
@@ -65,45 +85,31 @@ heading_e rotate_left(heading_e heading, rotation_e rotation) {
     return static_cast<heading_e>(h);
 }
 
-void navigate(ship_t& ship, control_t control) {
+void navigate(ship_t& ship, waypoint_t& waypoint, control_t control) {
     switch (control.action)
     {
     case 'N':
-        ship.y += control.value;
+        waypoint.y += control.value;
         break;
     case 'S':
-        ship.y -= control.value;
+        waypoint.y -= control.value;
         break;
     case 'E':
-        ship.x += control.value;
+        waypoint.x += control.value;
         break;
     case 'W':
-        ship.x -= control.value;
+        waypoint.x -= control.value;
         break;
     case 'R':
-        ship.heading = rotate_right(ship.heading, rotation(control.value));
+        waypoint = rotate(control.value, -1, waypoint.x, waypoint.y);
         break;
     case 'L':
-        ship.heading = rotate_left(ship.heading, rotation(control.value));
+        waypoint = rotate(control.value, 1, waypoint.x, waypoint.y);
         break;
     case 'F':
-        switch (ship.heading)
-        {
-        case heading_e::east:
-            ship.x += control.value;
-            break;
-        case heading_e::south:
-            ship.y -= control.value;
-            break;
-        case heading_e::west:
-            ship.x -= control.value;
-            break;
-        case heading_e::north:
-            ship.y += control.value;
-            break;
-        case heading_e::all_end:
-            break;
-        }
+        ship.x += waypoint.x * control.value;
+        ship.y += waypoint.y * control.value;
+        break;
     default:
         break;
     }
@@ -111,6 +117,10 @@ void navigate(ship_t& ship, control_t control) {
 
 int main(int argc, char** argv) {
     std::ifstream reader("input.txt");
+
+    // waypoint_t waypoint {1, 0};
+    // auto wp = rotate(90, 1, 1, 0);
+    // std::cout << wp.x << ":" << wp.y << "\n";
 
     std::vector<std::string> lines;
     for(std::string line; std::getline(reader, line);) {
@@ -124,13 +134,13 @@ int main(int argc, char** argv) {
         controls.push_back(control_t{action[0], stoi(value)});
     }
 
+    waypoint_t waypoint {10, 1};
     ship_t ship{heading_e::east, 0, 0};
     for (const auto control : controls) {
-        std::cout << control.action << ":" << control.value << "\n";
-        navigate(ship, control);
+        navigate(ship, waypoint, control);
     }
 
-    std::cout << "part1: " << std::abs(ship.x) + std::abs(ship.y) << "\n";
+    std::cout << "part2: " << std::abs(ship.x) + std::abs(ship.y) << "\n";
 
     return 0;
 }
