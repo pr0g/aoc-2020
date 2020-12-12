0ec75b9006de92f602ac202d27fdbdce727d50f7
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-12 11:59:54 +0000

    day 11 part 1

--- day11/main.cpp
+++ day11/main.cpp
@@ -18,10 +18,6 @@ int main(int argc, char** argv) {
         lines.push_back(line);
     }
 
-    for (const auto& line : lines) {
-        std::cout << line << "\n";
-    }
-
     std::vector<std::vector<char>> grid;
     for (const auto& line : lines) {
         grid.emplace_back();
@@ -29,8 +25,6 @@ int main(int argc, char** argv) {
             grid.back().push_back(symbol);
         }
     }
-
-    std::cout << "---\n";
 
     for (int row = 0; row < grid.size(); ++row) {
         for (int col = 0; col < grid.back().size(); ++col) {
@@ -40,8 +34,8 @@ int main(int argc, char** argv) {
     }
 
     struct offset_t {
-        int x;
-        int y;
+        int r;
+        int c;
     };
 
     struct change_t {
@@ -59,37 +53,55 @@ int main(int argc, char** argv) {
         std::vector<change_t> seats;
         offset_t start{row, col};
         for (const auto offset : offsets) {
-            const offset_t next_offset = {start.x + offset.x, start.y + offset.y};
-            if (next_offset.x >= 0 && next_offset.x < width
-                && next_offset.y >= 0 && next_offset.y < height) {
-                seats.push_back(change_t{next_offset.x, next_offset.y, grid[row][col]});
+            const offset_t next_offset = {start.r + offset.r, start.c + offset.c};
+            if (next_offset.r >= 0 && next_offset.r < height
+                && next_offset.c >= 0 && next_offset.c < width) {
+                seats.push_back(change_t{next_offset.r, next_offset.c, grid[next_offset.r][next_offset.c]});
             }
         }
-
         return seats;
     };
 
-    std::vector<change_t> changes;
-    for (int row = 0; row < grid.size(); ++row) {
-        for (int col = 0; col < grid.back().size(); ++col) {
-            const auto adjacent_seats =
-                adjacent_seats_fn(row, col, grid.back().size(), grid.size());
-            const auto occupied = std::count_if(
-                adjacent_seats.begin(), adjacent_seats.end(),
-                [](const auto change) {
-                return change.symbol == '#';
-            });
-            if (grid[row][col] == 'L' && occupied == 0) {
-                changes.push_back(change_t{row, col, '#'});
-            }
-            if (grid[row][col] == '#' && occupied >= 4) {
-                changes.push_back(change_t{row, col, 'L'});
+    int iterations = 0;
+    for (bool iterating = true; iterating;) {
+        std::vector<change_t> changes;
+        for (int row = 0; row < grid.size(); ++row) {
+            for (int col = 0; col < grid.back().size(); ++col) {
+                const auto adjacent_seats =
+                    adjacent_seats_fn(row, col, grid.back().size(), grid.size());
+                const auto occupied = std::count_if(
+                    adjacent_seats.begin(), adjacent_seats.end(),
+                    [](const auto change) {
+                    return change.symbol == '#';
+                });
+                if (grid[row][col] == 'L' && occupied == 0) {
+                    changes.push_back(change_t{row, col, '#'});
+                }
+                if (grid[row][col] == '#' && occupied >= 4) {
+                    changes.push_back(change_t{row, col, 'L'});
+                }
             }
         }
-    }
 
-    for (const auto change : changes) {
-        grid[change.row][change.col] = change.symbol;
+        for (const auto change : changes) {
+            grid[change.row][change.col] = change.symbol;
+        }
+
+        // std::cout << "---\n";
+
+        // // debug
+        // for (int row = 0; row < grid.size(); ++row) {
+        //     for (int col = 0; col < grid.back().size(); ++col) {
+        //         std::cout << grid[row][col];
+        //     }
+        //     std::cout << "\n";
+        // }
+
+        if (changes.empty()) {
+            iterating = false;
+        } else {
+            iterations++;
+        }
     }
 
     std::cout << "---\n";
@@ -100,6 +112,20 @@ int main(int argc, char** argv) {
         }
         std::cout << "\n";
     }
+
+    int occupied_seats = 0;
+    for (auto & row : grid) {
+        occupied_seats += std::count_if(
+            row.begin(), row.end(),
+            [](const auto change) {
+            return change == '#';
+        });
+    }
+
+    std::cout << "---\n";
+
+    std::cout << "iterations: " << iterations << "\n";
+    std::cout << "part1: " << occupied_seats << "\n";
 
     return 0;
 }
