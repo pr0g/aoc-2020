ac3a35a8eb6abb498b8c885b5c2060104e82766b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-23 22:38:12 +0000

    day 22 part 2 partial

--- day22/main.cpp
+++ day22/main.cpp
@@ -44,26 +44,70 @@ int main(int argc, char **argv)
         std::cout << card << '\n';
     }
 
+    struct round_t {
+        std::deque<int64_t> player1;
+        std::deque<int64_t> player2;
+    };
+
+    enum class winner_e {
+        none,
+        player1,
+        player2
+    };
+
+    winner_e winner = winner_e::none;
+    std::vector<round_t> rounds;
     while (!player1.empty() && !player2.empty()) {
+        if (!rounds.empty()) {
+            if (auto found = std::find_if(rounds.begin(), rounds.end(), [&](const round_t& round) {
+                return round.player1 == player1 && round.player2 == player2;
+            }); found != rounds.end()) {
+                winner = winner_e::player1;
+                break;
+            }
+        }
+
+        rounds.push_back({player1, player2});
+
+        // draw top card as normal
         auto p1_card = player1.front();
         player1.pop_front();
         auto p2_card = player2.front();
         player2.pop_front();
 
-        if (p1_card > p2_card) {
-            player1.push_back(p1_card);
-            player1.push_back(p2_card);
+        if (player1.size() >= p1_card && player2.size() > p2_card) {
+            auto player1_copy = std::vector<int64_t>{};
+            std::copy_n(player1.begin(), p1_card, std::back_inserter(player1_copy));
+            auto player2_copy = std::vector<int64_t>{};
+            std::copy_n(player2.begin(), p2_card, std::back_inserter(player2_copy));
+            // recursive combat
+
         } else {
-            player2.push_back(p2_card);
-            player2.push_back(p1_card);
+            if (p1_card > p2_card) {
+                player1.push_back(p1_card);
+                player1.push_back(p2_card);
+            } else {
+                player2.push_back(p2_card);
+                player2.push_back(p1_card);
+            }
         }
     }
 
-    if (player1.size() > player2.size()) {
+    if (winner == winner_e::player1) {
         player = &player1;
-    } else {
+    } else if (winner == winner_e::player2) {
         player = &player2;
+    } else {
+        int i;
+        i = 0; // oh no
     }
+
+//    part 1
+//    if (player1.size() > player2.size()) {
+//        player = &player1;
+//    } else {
+//        player = &player2;
+//    }
 
     int64_t mul = 1;
     int64_t score = 0;
