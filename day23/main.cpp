c6deaa31c67f2870c26cf031ce031cfbcf381a64
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-29 17:50:04 +0000

    day 23 part 2 tidy

--- day23/main.cpp
+++ day23/main.cpp
@@ -26,34 +26,7 @@ std::vector<int64_t> splice_map(
         result.push_back(next->first);
         next = cup_circle.find(next->second);
     }
-    auto t = insert->first;
-    auto tt = insert->second;
-    auto nc = next->first;
-    auto nn = next->second;
     insert->second = next->first;
-    return result;
-}
-
-std::list<int64_t> splice_some(
-    std::list<int64_t>& list,
-    std::list<int64_t>::iterator pos, size_t n) {
-    std::list<int64_t> result;
-
-    auto next = pos;
-    auto nn = *next;
-    for (size_t count = 0; count < n; count++) {
-        result.push_back(*next);
-        next = std::next(next, 1);
-        nn = *next;
-        if (next == list.end()) {
-            next = list.begin();
-        }
-    }
-
-    list.erase(std::remove_if(list.begin(), list.end(), [&](const auto cup) {
-        return std::find(result.begin(), result.end(), cup) != result.end();
-    }), list.end());
-
     return result;
 }
 
@@ -61,12 +34,9 @@ int main(int argc, char **argv)
 {
     // test input
 //    std::unordered_map<int64_t, int64_t> all_cups{{3, 8},{8, 9},{9, 1},{1, 2},{2, 5},{5, 4},{4, 6},{6, 7},{7, 3}};
-    //                                             4     7     6     1     3     8     2    5      9
     std::unordered_map<int64_t, int64_t> all_cups{{4,7},{7,6},{6,1},{1,3},{3,8},{8,2},{2,5},{5,9},{9,4}};
     const auto min_elem = std::min_element(all_cups.begin(), all_cups.end());
     auto max_elem = std::max_element(all_cups.begin(), all_cups.end());
-    auto tom = max_elem->first;
-    auto tom2 = max_elem->second;
 
     int64_t max = max_elem->first;
     auto last = all_cups.find(9);
@@ -77,41 +47,16 @@ int main(int argc, char **argv)
     }
     last->second = 4;
 
-//    for (auto cup_it = all_cups.begin(); cup_it != all_cups.end(); ++cup_it) {
-//        cup_lookup[*cup_it] = cup_it;
-//    }
-
     max_elem = std::max_element(all_cups.begin(), all_cups.end());
     std::unordered_map<int64_t, int64_t> cup_circle{all_cups};
 
     int64_t turns = 0;
-    int64_t current_cup = 4; //cup_circle.front();
+    int64_t current_cup = 4;
     while (turns < 10000000) {
-//        auto current_cup_it = cup_lookup.find(current_cup);
-//        auto next_cup_it = ++current_cup_it->second;
-        auto current_cup_it = cup_circle.find(current_cup); //std::find(cup_circle.begin(), cup_circle.end(), current_cup);
-//        auto next_cup_it = cup_circle.find(current_cup_it->second);// std::next(current_cup_it, 1);
-        auto cc = current_cup_it->first;
-        auto nc = current_cup_it->second;
-//        auto nc1 = next_cup_it->first;
-//        auto nc2 = next_cup_it->second;
-//        if (next_cup_it == cup_circle.end()) {
-//            next_cup_it = cup_circle.begin();
-//        }
-//        auto t = *next_cup_it;
-        std::vector<int64_t> held_cups = splice_map(cup_circle, current_cup, 3); //splice_some(cup_circle, next_cup_it, 3);
+        auto current_cup_it = cup_circle.find(current_cup);
+        std::vector<int64_t> held_cups = splice_map(cup_circle, current_cup, 3);
 
         auto cup = cup_circle.find(current_cup);
-//        for (int64_t c = 0; c < cup_circle.size() - held_cups.size(); ++c) {
-//            std::cout << cup->first << ", ";
-//            cup = cup_circle.find(cup->second);
-//        }
-//        std::cout << std::endl;
-
-//        for (const auto cup : cup_circle) {
-//            std::cout << cup << ", ";
-//        }
-//        std::cout << std::endl;
 
         auto destination_cup = current_cup - 1;
         while (true) {
@@ -124,63 +69,23 @@ int main(int argc, char **argv)
                 }
             } else {
                 auto destination_in_circle = cup_circle.find(destination_cup);
-//                destination_in_circle = cup_circle.find(destination_in_circle->second);
-
-                auto dc = destination_in_circle->first;
                 auto dnc = destination_in_circle->second;
 
-                // auto ins = cup_circle.find(destination_in_circle->first);
                 auto old_next = destination_in_circle;
                 auto next = old_next;
                 next->second = held_cups[0];
                 auto last_held_cup = cup_circle.find(held_cups[2]);
-                auto t1 = last_held_cup->first;
-                auto tt1 = last_held_cup->second;
-                auto t = old_next->first;
-                auto tt = old_next->second;
-                last_held_cup->second = dnc;//old_next->second;
-                auto t2 = last_held_cup->first;
-                auto tt2 = last_held_cup->second;
-//                for (const auto held_cup : held_cups) {
-//                    next = cup_circle.find(destination_in_circle->second);
-//                }
-
-                cup = cup_circle.find(current_cup);
-//                for (int64_t c = 0; c < cup_circle.size(); ++c) {
-//                    std::cout << cup->first << ", ";
-//                    cup = cup_circle.find(cup->second);
-//                }
-//                std::cout << std::endl;
-
-//                cup_circle.insert(
-//                    destination_in_circle, held_cups.begin(), held_cups.end());
-
+                last_held_cup->second = dnc;
                 break;
             }
         }
 
         current_cup_it = cup_circle.find(current_cup);
-//        next_cup_it = cup_circle.find(current_cup_it->second);
-
-//        current_cup_it = std::find(cup_circle.begin(), cup_circle.end(), current_cup);
-//        next_cup_it = std::next(current_cup_it, 1);
-
-//        if (next_cup_it == cup_circle.end()) {
-//            next_cup_it = cup_circle.begin();
-//        }
         current_cup = current_cup_it->second;
         turns++;
     }
 
-    auto one = cup_circle.find(1); //std::find(cup_circle.begin(), cup_circle.end(), 1);
-
-//    auto after_one = ++one;
-//    auto another_after_one = ++after_one;
-//    int64_t total = *after_one * *another_after_one;
-
-//    std::cout << "after on: " << *after_one
-//              << " another after: " << *another_after_one
-//              << " total: " << total << '\n';
+    auto one = cup_circle.find(1);
 
     std::vector<int64_t> result;
     auto next = cup_circle.find(one->second);
@@ -190,23 +95,12 @@ int main(int argc, char **argv)
     }
 
     int64_t r = result[0] * result[1];
-    std::cout << r << '\n';
+    std::cout << "part 2: " << r << '\n';
 
     for (auto i : result) {
         std::cout << i << "\n";
     }
     std::cout << '\n';
-
-//    size_t counter = 0;
-//    for (auto offset = std::next(one, 1);
-//        counter < cup_circle.size() - 1; counter++) {
-//        std::cout << *offset;
-//        offset = std::next(offset, 1);
-//        if (offset == cup_circle.end()) {
-//            offset = cup_circle.begin();
-//        }
-//    }
-//    std::cout << '\n';
 
     return 0;
 }
