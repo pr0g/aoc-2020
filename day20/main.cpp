d2b130d138fa1e91837cddf5da4ba60cb1f37ad2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-12-31 22:48:22 +0000

    day 20 tidy-up

--- day20/main.cpp
+++ day20/main.cpp
@@ -33,9 +33,6 @@ int main(int, char**)
         return true;
     };
 
-//    using basis_t = std::vector<char>;
-//    using grid_t = std::vector<basis_t>;
-
     enum side_e {
         top,
         right,
@@ -63,41 +60,16 @@ int main(int, char**)
         image_t image;
 
         vec2_t dirs[4] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int dir_chks[4] = {0, 1, 2, 3};
-
-//        int s = 0;
-//        int f = 0;
-
-        // side direction
-
-//        bool flipped = false;
-//        int right_rotations = 0;
 
         void rotate_right() {
-//            right_rotations = (right_rotations + 1) % 4;
             std::swap(dirs[0], dirs[3]);
             std::swap(dirs[2], dirs[3]);
             std::swap(dirs[1], dirs[2]);
-
-            std::swap(dir_chks[0], dir_chks[3]);
-            std::swap(dir_chks[2], dir_chks[3]);
-            std::swap(dir_chks[1], dir_chks[2]);
         }
-//        void rotate_left() {
-////            right_rotations = (right_rotations + 1) % 4;
-//            std::swap(dirs[1], dirs[2]);
-//            std::swap(dirs[2], dirs[3]);
-//            std::swap(dirs[0], dirs[3]);
-//        }
+
         void flip() {
-//            flipped = !flipped;
             std::swap(dirs[1], dirs[3]);
-
-            std::swap(dir_chks[1], dir_chks[3]);
         }
-
-//        basis_t sides[4]{};
-//        basis_t orig_sides[4]{};
 
         [[nodiscard]] basis_t side(side_e side) const {
             switch (side) {
@@ -113,14 +85,6 @@ int main(int, char**)
             return basis_t{};
         }
 
-//        void reset() {
-//            for (int i = 0; i < 4; ++i) {
-//                for (int j = 0; j < 10; ++j) {
-////                    sides[i][j] = orig_sides[i][j];
-//                }
-//            }
-//        }
-
         bool operator==(const tile_t& tile) const {
             for (int i = 0; i < grid_t::size(); ++i) {
                 if (tile.grid[i] != grid[i]) {
@@ -128,14 +92,10 @@ int main(int, char**)
                 }
             }
             return true;
-            //            return sides[top] == tile.sides[top] &&
-//                sides[right] == tile.sides[right] &&
-//                sides[bottom] == tile.sides[bottom] &&
-//                sides[left] == tile.sides[left];
         }
     };
 
-    auto rotate_right = [print_dirs](const tile_t& tile_in) {
+    auto rotate_right = [](const tile_t& tile_in) {
         tile_t tile = tile_in;
         tile.rotate_right();
         tile.grid = as::mat_transpose(tile.grid);
@@ -144,32 +104,10 @@ int main(int, char**)
             std::reverse(&row[0], &row[0] + 10);
             as::mat_row(tile.grid, r, row);
         }
-//        std::cout << "rot - id:  " << tile.id << '\n';
-//        print_dirs(tile.dirs, 4);
-//        basis_t bases[] = {tile.sides[top], tile.sides[right], tile.sides[bottom], tile.sides[left]};
-//        tile.sides[top] = bases[left];
-//        std::reverse(tile.sides[top].begin(), tile.sides[top].end());
-//        tile.sides[right] = bases[top];
-//        tile.sides[bottom] = bases[right];
-//        std::reverse(tile.sides[bottom].begin(), tile.sides[bottom].end());
-//        tile.sides[left] = bases[bottom];
         return tile;
     };
 
-//    auto rotate_left = [](const tile_t& tile_in) {
-//        tile_t tile = tile_in;
-//        tile.rotate_left();
-//        basis_t bases[] = {tile.sides[top], tile.sides[right], tile.sides[bottom], tile.sides[left]};
-//        tile.sides[top] = bases[right];
-//        tile.sides[right] = bases[bottom];
-//        std::reverse(tile.sides[right].begin(), tile.sides[right].end());
-//        tile.sides[bottom] = bases[left];
-//        tile.sides[left] = bases[top];
-//        std::reverse(tile.sides[left].begin(), tile.sides[left].end());
-//        return tile;
-//    };
-
-    auto flip_horizontal = [print_dirs](const tile_t& tile_in) {
+    auto flip_horizontal = [](const tile_t& tile_in) {
         tile_t tile = tile_in;
         tile.flip();
         for (int r = 0; r < 10; ++r) {
@@ -177,25 +115,8 @@ int main(int, char**)
             std::reverse(&row[0], &row[0] + 10);
             as::mat_row(tile.grid, r, row);
         }
-//        std::cout << "flip - id:  " << tile.id << '\n';
-//        print_dirs(tile.dirs, 4);
-//        basis_t bases[] = {tile.sides[right], tile.sides[left]};
-//        tile.sides[left] = bases[top];
-//        tile.sides[right] = bases[right];
-//        std::reverse(tile.sides[top].begin(), tile.sides[top].end());
-//        std::reverse(tile.sides[bottom].begin(), tile.sides[bottom].end());
         return tile;
     };
-
-//    auto flip_vertical = [](const tile_t& tile_in) {
-//        tile_t tile = tile_in;
-//        basis_t bases[] = {tile.sides[top], tile.sides[bottom]};
-//        tile.sides[bottom] = bases[top];
-//        tile.sides[top] = bases[right];
-//        std::reverse(tile.sides[left].begin(), tile.sides[left].end());
-//        std::reverse(tile.sides[right].begin(), tile.sides[right].end());
-//        return tile;
-//    };
 
     auto print_grid = [](const tile_t& tile){
         std::cout << "---\n";
@@ -231,10 +152,8 @@ int main(int, char**)
     int grid_row = 0;
     mode_e mode = mode_e::title;
     std::vector<tile_t> tiles;
-    for (std::string line; std::getline(reader, line);)
-    {
-        if (line.empty())
-        {
+    for (std::string line; std::getline(reader, line);) {
+        if (line.empty()) {
             tiles.push_back(active_tile);
             grid_row = 0;
             active_tile.id = -1;
@@ -242,8 +161,7 @@ int main(int, char**)
             continue;
         }
 
-        switch (mode)
-        {
+        switch (mode) {
         case mode_e::title: {
             auto id_off = line.find(' ');
             auto colon_off = line.find(':');
@@ -254,14 +172,11 @@ int main(int, char**)
         }
         break;
         case mode_e::grid: {
-//            std::vector<char> row;
             as::vec<char, 10> row;
-//            for (const auto c : line) {
             for (int i = 0; i < as::vec<char, 10>::size(); ++i) {
-//                row.push_back(c);
                 row[i] = line[i];
             }
-            as::mat_row(active_tile.grid, grid_row++, row); // active_tile.grid.(row);
+            as::mat_row(active_tile.grid, grid_row++, row);
         }
         break;
         }
@@ -280,25 +195,6 @@ int main(int, char**)
             std::cout << '\n';
         }
     }
-
-//    for (auto& tile : tiles) {
-//        tile.sides[top] = tile.grid[0];
-//        tile.sides[bottom] = tile.grid[9];
-
-//        basis_t left, right;
-//        for (const auto& row : tile.grid) {
-//            left.push_back(row[0]);
-//            right.push_back(row[9]);
-//        }
-//        tile.sides[side_e::left] = left;
-//        tile.sides[side_e::right] = right;
-
-//        for (int i = 0; i < 4; ++i) {
-//            for (int j = 0; j < 10; ++j) {
-//                tile.orig_sides[i] = tile.sides[i];
-//            }
-//        }
-//    }
 
     std::deque<tile_t> available_tiles(tiles.begin(), tiles.end());
     std::vector<tile_t> pinned_tiles;
@@ -327,8 +223,6 @@ int main(int, char**)
     std::vector<int64_t> corner_ids;
     while (!available_tiles.empty()) {
         for (int t = 0; t < available_tiles.size(); ++t) {
-    //        std::unordered_map<int, tile_position_t> matching;
-    //        for (auto& pinned_tile : pinned_tiles) {
             for (int64_t pinned_tile_index = 0; pinned_tile_index < pinned_tiles.size(); ++pinned_tile_index) {
                 for (int64_t tile_index = 0; tile_index < available_tiles.size(); ++tile_index) {
                     for (int s = 0; s < 4; ++s) {
@@ -339,19 +233,7 @@ int main(int, char**)
                                     pinned_tiles[pinned_tile_index].side((side_e)s),
                                     available_tiles[tile_index].side((side_e)opposite_side))) {
 
-//                                    for (int i = 0; i < s; ++i) {
-//                                        available_tiles[tile_index] = rotate_left(available_tiles[tile_index]);
-//                                    }
-
                                     pinned_tiles.push_back(available_tiles[tile_index]);
-
-//                                    std::cout << "()()()()" << "\n";
-//                                    print_grid(pinned_tiles[pinned_tile_index]);
-//                                    print_grid(pinned_tiles.back());
-//                                    std::cout << "()()()()" << std::endl;
-
-//                                    pinned_tiles.back().s = s;
-//                                    pinned_tiles.back().f = f;
 
                                     available_tiles[tile_index] = available_tiles.back();
                                     available_tiles.pop_back();
@@ -361,50 +243,12 @@ int main(int, char**)
                                         return pinned_tiles[pinned_tile_index].id == tile_pos.id;
                                     });
 
-//                                    std::cout << "pinned - id:  " << pinned_tiles[pinned_tile_index].id << '\n';
-//                                    print_dirs(pinned_tiles[pinned_tile_index].dirs, 4);
-//                                    std::cout << "next - id:  " << pinned_tiles.back().id << '\n';
-//                                    print_dirs(pinned_tiles.back().dirs, 4);
-
-//                                    int current_s = s + pinned_tiles[pinned_tile_index].right_rotations;
-//                                    if (pinned_tiles[pinned_tile_index].flipped) {
-//                                        if (current_s == left) {
-//                                            current_s = right;
-//                                        } else if (current_s == right) {
-//                                            current_s = left;
-//                                        }
-//                                    }
-
-//                                    current_s = (current_s + 4 - pinned_tiles[pinned_tile_index].right_rotations) % 4;
-
-
-                                    auto next_s = (s + 4 + r) % 4;
-
                                     position_t next_pos = found->pos;
                                     next_pos.x += pinned_tiles[0].dirs[s].x;
                                     next_pos.y += pinned_tiles[0].dirs[s].y;
 
-//                                    if (current_s == left) {
-//                                        next_pos.x -= 1;
-//                                    }
-//                                    if (current_s == right) {
-//                                        next_pos.x += 1;
-//                                    }
-//                                    if (current_s == top) {
-//                                        next_pos.y += 1;
-//                                    }
-//                                    if (current_s == bottom) {
-//                                        next_pos.y -= 1;
-//                                    }
-
                                     tile_positions.push_back({pinned_tiles.back().id, next_pos});
-//                                    available_tiles[tile_index].reset();
                                     goto loop;
-
-    //                                auto found = matching.find(tile_index);
-    //                                if (found == matching.end()) {
-    //                                    found = matching.insert({tile_index, tile_position_t{}}).first;
-    //                                }
                                 }
                                 available_tiles[tile_index] = rotate_right(available_tiles[tile_index]);
                             }
@@ -413,13 +257,6 @@ int main(int, char**)
                     }
                 }
             }
-
-//        if (matching.size() == 2) {
-//            corner_ids.push_back(pinned_tiles.front().id);
-//        }
-
-//        available_tiles.push_back(pinned_tiles.front());
-//        pinned_tiles.pop_back();
         }
 loop:
         (void)sizeof(int);
@@ -490,11 +327,6 @@ loop:
     }
 
     std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-
-    // ---
 
     constexpr as::index full_image_size = 96;
     using full_image_t = as::mat<char, full_image_size>;
@@ -527,9 +359,8 @@ loop:
             row++;
         }
     }
-    std::cout << std::endl;
 
-    // ---
+    std::cout << std::endl;
 
     auto print_img = [](const full_image_t& image) {
         for (int r = 0; r < full_image_size; r++) {
